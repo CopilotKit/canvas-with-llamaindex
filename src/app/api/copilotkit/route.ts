57711b0ca607636a38732300e05bdd8cd4133b8c
Brandon McConnell <brandon@dreamthinkbuild.com>
2025-09-23 13:08:48 -0400

    chore: restructure `route.ts` to match structure in other templates

--- src/app/api/copilotkit/route.ts
+++ src/app/api/copilotkit/route.ts
@@ -4,22 +4,26 @@ import {
   copilotRuntimeNextJSAppRouterEndpoint,
 } from "@copilotkit/runtime";
 import { LlamaIndexAgent } from "@ag-ui/llamaindex";
-
 import { NextRequest } from "next/server";
 
-export async function POST(request: NextRequest) {
- 
-  const runtime = new CopilotRuntime({
-    agents: {
-      sample_agent: new LlamaIndexAgent({
-        url: "http://127.0.0.1:9000/run",
-      })
-    }
-  })
+// 1. You can use any service adapter here for multi-agent support. We use
+//    the empty adapter since we're only using one agent.
+const serviceAdapter = new ExperimentalEmptyAdapter();
 
+// 2. Create the CopilotRuntime instance and utilize the LlamaIndex AG-UI
+//    integration to setup the connection.
+const runtime = new CopilotRuntime({
+  agents: {
+    sample_agent: new LlamaIndexAgent({
+      url: "http://127.0.0.1:9000/run",
+    })
+  }
+})
+
+export async function POST(request: NextRequest) {
   const { handleRequest } = copilotRuntimeNextJSAppRouterEndpoint({
     runtime,
-    serviceAdapter: new ExperimentalEmptyAdapter(),
+    serviceAdapter,
     endpoint: `/api/copilotkit`,
   });
 
